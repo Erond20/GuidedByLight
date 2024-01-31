2dac30948bc9ab155169b4cae4937800a52f3891
Jani Palo <fazermusta@gmail.com>
2024-01-31 11:17:23 +0200

    EnemySpriteDone

---
+++ Assets/Scripts/PlayerAim.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+using UnityEngine.EventSystems;
+
+public class PlayerAim : MonoBehaviour
+{
+    public Transform aimTarget;
+    private Vector2 aimDirection;
+
+    void Update(){
+        aimDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        aimDirection = aimDirection.normalized;
+
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+
+        transform.rotation = Quaternion.Euler(0,0,aimAngle);
+    }
+}
