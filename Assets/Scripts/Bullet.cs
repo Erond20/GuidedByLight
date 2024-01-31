16e8df3bd312cf2aa0e2a572d819aef5220c535d
Jani Palo <fazermusta@gmail.com>
2024-01-31 14:56:40 +0200

    Small changes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public int enemyCount = 0;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
+            enemyCount++;
             Destroy(collision.gameObject);
             PlayerShooting.instance.lightArea.pointLightOuterRadius += .5f;
             Destroy(this.gameObject, 2f);
